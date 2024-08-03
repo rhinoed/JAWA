e6e14df5b0c983f88a3606f47f248657fee3f2b2
Mark Edmunds <edmundsmd1@gmail.com>
2024-08-02 23:02:38 -0700

    Add two ObjectProperties
    weather_binding
    	- instance of `Weather` used to get and ouput weather
    user_prefs
    	- instance of `UserPreferences` used to save to user_prefs

--- ModalView.py
+++ ModalView.py
@@ -4,11 +4,27 @@
 
 # Import the necessary libraries
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
 
 class AddFavorite(BoxLayout):
-    
+    weather_binding = ObjectProperty(None)
+    user_prefs = ObjectProperty(None)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def dismiss(self):
+    def add_then_dismiss(self, input_text):
+        try:
+            # get the weather data for the city
+            self.weather_binding.search_button_pressed(input_text)
+            # add the city to the favorites
+            self.user_prefs.add_to_favorites(self.weather_binding.city)
+        except IndexError as ie:
+            self.parent.title = f"An error occurred while adding favorite: {ie}"
+            print(f"An error occurred while adding favorite: {ie}")
+        
+        # output the weather this is actually done twice once when `search_button_pressed` is called and again here
+        # this is done to ensure the favorites are updated
+        if input_text != "":
+            self.weather_binding.output_weather()
         self.parent_popup.dismiss()   
